2948981c8ce90e0f311e5c170c4fadedc40f461a
NikolayTach <42199859+NikolayTach@users.noreply.github.com>
2021-02-25 21:30:36 -0800

    Update Namo.ts

--- jml77.js-master/Nomi-main/Namo.ts
+++ jml77.js-master/Nomi-main/Namo.ts
@@ -26,3 +26,24 @@ import node demand locking mix of fork node = [vc0r] aplitude next node
 await sm_left patterns [ir?2Ban] | nnhawait sm_right parrens [ols@%2bc] | nnh
     }
   
+import  pdf file arools all
+   [...]
+await loop on large mark [set] add (--.2-.0)dor bost to (L<0;lim)/ [vc0r].
+boost while ms cdd 
+import  pdf file arools all
+   [...]      
+await loop on large mark [set] add (--.2-.0)dor bost to (L<0;lim)/ [vc0r].
+boost while ms cdd
+
+#File is set under the provided manual name of "[vc0r]" added episode locked bost menu fold#
+    }
+import from host image node on to frame binded folded locks copied lines [cr176483029@git]
+set dmode 
+set as natural folding "42" nominal set ms cdd 
+
+    {
+
+import node demand locking mix of fork node = [vc0r] aplitude next node 
+await sm_left patterns [ir?2Ban] | nnhawait sm_right parrens [ols@%2bc] | nnh
+    }
+  
